87f925c6560f018bc1954f57d045a62fd771d750
将军℡ <76241047+General74110@users.noreply.github.com>
2020-12-26 19:57:38 +0800

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -1,5 +1,4 @@
-
-https://weather-data.apple.com/v1/weather/[\w-_]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include= url script-response-body https://cdn.jsdelivr.net/gh/Hackl0us/SS-Rule-Snippet@master/Scripts/Surge/iOS_Weather_AQI_Standard.js
+^https://weather-data.apple.com/v1/weather/[\w-_]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include= url script-response-body https://cdn.jsdelivr.net/gh/Hackl0us/SS-Rule-Snippet@master/Scripts/Surge/iOS_Weather_AQI_Standard.js
 
 hostname = weather-data.apple.com
 
@@ -12,7 +11,7 @@ const AirQualityStandard = {
 let body = JSON.parse($response.body); 
 
 // 选定 AQI 标准(默认 AQI-US)
-body.air_quality.airQualityScale = AirQualityStandard.CN;
+body.air_quality.airQualityScale = AirQualityStandard.US;
 
 body = JSON.stringify(body);
 $done({body});
