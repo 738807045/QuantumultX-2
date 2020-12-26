6a0ae4f0f56f89a85a904b209df2c163fc37ba0b
将军℡ <76241047+General74110@users.noreply.github.com>
2020-12-26 20:07:50 +0800

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -1,6 +1,3 @@
-//https://weather-data.apple.com/v1/weather/[\w-_]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include= url script-response-body https://raw.githubusercontent.com/General74110/QuantumultX/main/Weather.js
-hostname = weather-data.apple.com
-
 // AQI 标准
 const AirQualityStandard = {
 	CN: "HJ6332012.1",
