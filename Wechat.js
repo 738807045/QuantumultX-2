896a5e819323178872f18c36548e994e680eca5e
将军℡ <76241047+General74110@users.noreply.github.com>
2020-12-27 03:32:19 +0800

    Update Wechat.js

--- Wechat.js
+++ Wechat.js
@@ -1,11 +1,6 @@
 /*
-Remove the WeChat public account bottom ad
-by Choler
 QX:
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/General74110/QuantumultX/main/Wechat.js?token=ASFVRF44GRKCVAMBYWWJJ2K73XOH2
-
-Surge4：
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/General74110/QuantumultX/main/Wechat.js
 
 Surge & QX MITM = mp.weixin.qq.com,
 */
