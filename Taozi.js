7cc9b7bc8af17f4257a6da4ad314f3ce63432e77
将军℡ <76241047+General74110@users.noreply.github.com>
2020-12-28 15:17:06 +0800

    Update Taozi.js

--- Taozi.js
+++ Taozi.js
@@ -8,11 +8,33 @@ hostname = api.06f158e.xyz
 */
 
 obj= {
-  "vipType\": 2",
-  "vipValidDate\": 2240279057000",
-  "accountLevel\": 2",
-  "gold\": 99999",
-
-  }
+  "code": "0000",
+  "message": "",
+  "total": null,
+  "result": {
+    "id": 3230372,
+    "account": "20204611479",
+    "phone": "20204611479",
+    "accountLevel": 2,
+    "machineCode": "iPhone 11 Pro Max|14.3",
+    "accessToken": "66ZO5UFMFLU5DWSPFGBWJRF68Q01JXOV",
+    "tokenValidDate": 1611676800000,
+    "vipValidDate": 2240279057000,
+    "sex": null,
+    "nickName": null,
+    "avatar": null,
+    "invitationCode": "FEFCFJE",
+    "channelCode": null,
+    "createDate": 1609130828000,
+    "gold": 99999,
+    "vipType": 2,
+    "authType": 0,
+    "bindStatus": 0,
+    "fans": 0,
+    "stars": 0,
+    "newUser": false
+  },
+  "data": null
+}
 body = JSON.stringify(obj);
 $done(body);
