dbd31486c417d1057485216a7643c2e9e94d88d5
将军℡ <76241047+General74110@users.noreply.github.com>
2021-01-09 22:45:55 +0800

    Update TikTok.js

--- TikTok.js
+++ TikTok.js
@@ -1,6 +1,6 @@
 #TikTok18.21
 
-hostname = *.tiktokv.com,*.byteoversea.com,*.tik-tokapi.com
+#hostname = *.tiktokv.com,*.byteoversea.com,*.tik-tokapi.com
 
 (?<=_region=)CN(?=&) url 307 JP
 (?<=&mcc_mnc=)4 url 307 2
