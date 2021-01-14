afa770dc9ae042479232383f02901ee4e11678ce
将军℡ <76241047+General74110@users.noreply.github.com>
2021-01-14 12:31:23 +0800

    Update Weread.js

--- Weread.js
+++ Weread.js
@@ -9,34 +9,31 @@ hostname:i.weread.qq.com
 */
 var obj = JSON.parse($response.body);
 obj= {
-  "startTime": 991603940132,
-  "expiredTime": 991604246399,
+  "startTime": 1585386150,
+  "expiredTime": 3043041983,
   "expired": 0,
   "isPaying": 0,
   "permanent": 0,
-  "day": 91,
-  "remainTime": 99306196,
-  "payingRemainTime": 6,
-  "isAutoRenewable": 6,
-  "historyAutoRenewable": 6,
-  "autoRenewableChannel": 6,
-  "autoRenewableTime": 6,
-  "autoRenewablePrice": 991900,
-  "savedMoney": 9927433,
-  "totalFreeReadDay": 6666,
-  "remainCoupon": 6666,
-  "remainCount": 6666,
+  "day": 9,
+  "remainTime": 11111057862,
+  "isAutoRenewable": 0,
+  "historyAutoRenewable": 0,
+  "autoRenewableChannel": 0,
+  "autoRenewableTime": 0,
+  "autoRenewablePrice": 1900,
+  "savedMoney": 3000,
+  "totalFreeReadDay": 0,
+  "remainCoupon": 0,
+  "remainCount": 0,
   "hintsForRecharge": {
-    "predictedSavedMoney": 9910315,
+    "predictedSavedMoney": 10315,
     "predictedChapterPrice": 15,
-    "pricePerMonth": 99900,
-    "sendCoupons": 6666,
-    "buttonTitle": "付费无限卡9元/月 · 限时特惠",
-    "buttonSubtitle": "你正在使用免费无限卡 · 升级付费无限卡即可阅读"
+    "pricePerMonth": 900,
+    "sendCoupons": 0
   },
-  "freeBookIds": ["352681"],
+  "freeBookIds": ["352681"]
   "timestamp": 1603940203,
   "random": 8341,
   "signature": "5d7f6c929086a4a958ead60c9bd95fae6991afef51c1de50a41e7c2641fdaf3f"
-}
+  }
 $done({body: JSON.stringify(obj)});
