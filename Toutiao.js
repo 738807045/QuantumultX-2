b0d42397cbd0084a9b7ab4cf77259ba38556250c
将军℡ <76241047+General74110@users.noreply.github.com>
2021-01-22 20:05:46 +0800

    Update Toutiao.js

--- Toutiao.js
+++ Toutiao.js
@@ -9,7 +9,7 @@ hostname = *.snssdk.com
 var obj = JSON.parse($response.body);
 if (obj.data) {
   for (var i = obj.data.length - 1; i >= 0; i--) {
-    if (obj.data[i].content.indexOf("raw_ad_data") > 0) {
+    if (obj.data[i].content.indexOf("raw_ad_data") > -1) {
       obj.data.splice(i, 1);
     }
   }
