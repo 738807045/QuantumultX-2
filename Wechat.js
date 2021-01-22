add800ee5468883d6e72cc7383146e3b9159a6be
将军℡ <76241047+General74110@users.noreply.github.com>
2021-01-22 18:19:50 +0800

    Update Wechat.js

--- Wechat.js
+++ Wechat.js
@@ -11,7 +11,7 @@ Surge & QX MITM = mp.weixin.qq.com,
 */
 
 var obj = JSON.parse($response.body);
-obj.advertisement_num = 0;
+obj.advertisement_num = 1;
 obj.advertisement_info = [];
 delete obj.appid;
 $done({body: JSON.stringify(obj)}); 
