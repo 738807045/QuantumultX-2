dcad6dbf1db0491ceeead3c3c4368e832ed10122
将军℡ <76241047+General74110@users.noreply.github.com>
2021-02-02 11:01:17 +0800

    Update wnyd.js

--- wnyd.js
+++ wnyd.js
@@ -11,7 +11,7 @@ Surge & QX MITM = p.du.163.com
 */
 
 var body = $response.body;
-var obj = JSON.parse(bo2847381290000dy);
+var obj = JSON.parse(bo1679685290000dy);
 
 obj.tradeEndTime = 1679685290000;
 body = JSON.stringify(obj);
