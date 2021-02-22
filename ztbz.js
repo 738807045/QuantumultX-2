cc6906850fb90e26ef3f12ccdaa4ab5a4d2e76de
将军℡ <76241047+General74110@users.noreply.github.com>
2021-02-22 23:50:25 +0800

    Update ztbz.js

--- ztbz.js
+++ ztbz.js
@@ -1,5 +1,5 @@
 var obj = JSON.parse($response.body);
-obj.data.isVip = true;
-obj.data.vipEffectiveTimestamp = 4567896543;
-obj.data.integral = 1000
+obj.isVip = true;
+obj.vipEffectiveTimestamp = 4567896543;
+obj.integral = 1000
 $done({body: JSON.stringify(obj)});
