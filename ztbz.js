5e1abcef47fb01c42cfbc2459e469cdf4d2b52c2
将军℡ <76241047+General74110@users.noreply.github.com>
2021-02-22 23:35:07 +0800

    Update ztbz.js

--- ztbz.js
+++ ztbz.js
@@ -1,9 +1,9 @@
-*/
+/*
 纸糖壁纸下载链接https://apps.apple.com/cn/app/纸塘壁纸/id1483440137
 hostname = api.bspapp.com
 
 rewrite_local：^https?:\/\/api\.bspapp\.com\/client url script-response-body ztbz.js
-
+*/
 
 var obj = JSON.parse($response.body);
 obj= {
