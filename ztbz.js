f1811d232cb5b81d5a9a46375f4ff1e44ee9ad0e
将军℡ <76241047+General74110@users.noreply.github.com>
2021-02-22 23:32:53 +0800

    Update ztbz.js

--- ztbz.js
+++ ztbz.js
@@ -7,34 +7,8 @@ rewrite_local：^https?:\/\/api\.bspapp\.com\/client url script-response-body zt
 
 var obj = JSON.parse($response.body);
 obj= {
-  "success": true,
-  "data": {
-    "code": 200,
-    "msg": "请求成功",
-    "data": {
-      "_id": "602ebdcffac28b0001b75c0a",
-      "id": "602ebdcffac28b0001b75c0a",
-      "isVip": true,
-      "vipEffectiveTimestamp": 4567896543,
-      "integral": 1,
-      "avatar": "",
-      "nickName": "将军",
-      "rewardCode": ""
-    }
-  },
-  "header": {
-    "x-fc-code-checksum": "11920753897763040351",
-    "access-control-expose-headers": "Date,x-fc-request-id,x-fc-error-type,x-fc-code-checksum,x-fc-invocation-duration,x-fc-max-memory-usage,x-fc-log-result,x-fc-invocation-code-version",
-    "x-fc-invocation-service-version": "LATEST",
-    "content-disposition": "attachment",
-    "x-serverless-request-id": "ac1412331614005901269476674",
-    "x-serverless-runtime-version": "1.1.1",
-    "content-length": "234",
-    "date": "Mon, 22 Feb 2021 14:58:21 GMT",
-    "x-fc-request-id": "4688e9e1-d494-4b1d-9bcc-af0af27ecc0d",
-    "x-fc-invocation-duration": "207",
-    "x-fc-max-memory-usage": "44.11",
-    "content-type": "application/json"
-  }
+"isVip": true,
+"vipEffectiveTimestamp": 4567896543,
+"integral": 1000,
 }
 $done({body: JSON.stringify(obj)});
